6e1d5606a4513e757e3363a44f1cf3dee934e615
Dushee2006 <dulashabhanuki9@gmail.com>
2026-03-18 13:04:04 +0530

    added temporary google tts with webcam testing for model validation

--- vision/currency_detection/src/currency_detector.py
+++ vision/currency_detection/src/currency_detector.py
@@ -1,10 +1,35 @@
 from ultralytics import YOLO
 import cv2
+from gtts import gTTS
+import os
+import time
 
 # load trained model
-model = YOLO("vision/currency_detection/models/best.pt")
+model = YOLO("../models/best.pt")
 
+# webcam
 cap = cv2.VideoCapture(0)
+
+last_spoken = ""
+last_time = 0
+
+
+def speak(text):
+    filename = "output.mp3"
+    
+    tts = gTTS(text=text, lang='en')
+    tts.save(filename)
+
+    # play audio (Windows)
+    os.system(f"start {filename}")
+
+    # small delay to avoid file lock issues
+    time.sleep(1)
+
+    # delete file
+    if os.path.exists(filename):
+        os.remove(filename)
+
 
 while True:
 
@@ -25,24 +50,35 @@ while True:
                 continue
 
             label = model.names[cls]
+            text = label.replace("_", " ")
 
             x1, y1, x2, y2 = map(int, box.xyxy[0])
 
-            # draw box
+            # draw bounding box
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
             # show label + confidence
-            text = f"{label} {conf:.2f}"
-            cv2.putText(frame, text, (x1, y1 - 10),
+            display_text = f"{text} {conf:.2f}"
+            cv2.putText(frame, display_text, (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+
+            current_time = time.time()
+
+            # avoid repeating same speech too often
+            if text != last_spoken or current_time - last_time > 3:
+                print(text)
+                speak(text)
+
+                last_spoken = text
+                last_time = current_time
 
     cv2.imshow("Currency Detection - Test Mode", frame)
 
-    # press q to exit
+    # press ESC or Q to exit
     key = cv2.waitKey(1) & 0xFF
-
     if key == 27 or key == ord('q'):
-       break
+        break
+
 
 cap.release()
 cv2.destroyAllWindows()
