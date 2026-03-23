a4696a7eb6598caaeaa1d0f7905225e0c34845d5
NightCode <savindulink@gmail.com>
2026-03-23 10:09:20 +0530

    Set the google cloud server IP

--- apps/user_mobile/lib/core/constants/api_constants.dart
+++ apps/user_mobile/lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   // Change this ONE string whenever you switch Wi-Fi networks!
-  static const String baseUrl = 'http://127.0.0.1:5000';
+  static const String baseUrl = 'http://34.136.33.37:5000';
   
   }
